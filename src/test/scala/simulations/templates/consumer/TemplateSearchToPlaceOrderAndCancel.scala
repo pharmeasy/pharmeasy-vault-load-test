b484519fb4cbd11ff5cd11252a8c3de6715e1de7
Sarath Kumar Purushothaman <sarath.purushothaman@pharmeasy.in>
2020-01-25 19:56:32 +0530

    parameterizing rampup user

--- src/test/scala/simulations/templates/consumer/TemplateSearchToPlaceOrderAndCancel.scala
+++ src/test/scala/simulations/templates/consumer/TemplateSearchToPlaceOrderAndCancel.scala
@@ -1,9 +1,10 @@
 package simulations.templates.consumer
 
+import java.lang.System._
+
 import actions.ConsumerActions._
 import io.gatling.core.Predef._
 import io.gatling.core.controller.inject.open.OpenInjectionStep
-import io.gatling.http.Predef._
 import simulations.templates.BaseTemplate
 import utils.Utilities._
 
@@ -11,49 +12,88 @@ import scala.concurrent.duration._
 
 class TemplateSearchToPlaceOrderAndCancel extends BaseTemplate {
 
+  private val rampUpUsers = getProperty("rampUpUsers", "1").trim.toInt
+  private val rampUpDuration = getProperty("rampUpDuration", "2").trim.toInt
+
   override def name() = "search - place order - cancel"
 
   val scn = scenario(name)
     .exec(session => {
       val randomPayment = randomPaymentMode()
-      addToSession(session, ("pageNumber", "1"), ("pageNumberMax", randomPageNumber), ("query", randomMedicineQuery), ("contactNumber", randomMobileNumber), ("paymentId", randomPayment("paymentId")), ("paymentSubId", randomPayment("paymentSubId")))
+      addToSession(
+        session,
+        ("pageNumber", "1"),
+        ("pageNumberMax", randomPageNumber),
+        ("query", randomMedicineQuery),
+        ("contactNumber", randomMobileNumber),
+        ("paymentId", randomPayment("paymentId")),
+        ("paymentSubId", randomPayment("paymentSubId"))
+      )
     })
-    .exec(recommendations()).exitHereIfFailed
-    .exec(home()).exitHereIfFailed
-    .exec(omniSearchCharByChar(query = randomMedicineQuery)).exitHereIfFailed
-    .exec(viewPages()).exitHereIfFailed
+    .exec(recommendations())
+    .exitHereIfFailed
+    .exec(home())
+    .exitHereIfFailed
+    .exec(omniSearchCharByChar(query = randomMedicineQuery))
+    .exitHereIfFailed
+    .exec(viewPages())
+    .exitHereIfFailed
     .exec(session => randomProductId(session))
-    .exec(productInformation()).exitHereIfFailed
-    .exec(productDescription()).exitHereIfFailed
-    .exec(addToCart()).exitHereIfFailed
-    .exec(recommendedOTCProducts()).exitHereIfFailed
-    .exec(savingsOnCart()).exitHereIfFailed
-    .exec(sendOTP()).exitHereIfFailed
-    .exec(login()).exitHereIfFailed
-    .exec(cartSyncAfterLogin()).exitHereIfFailed
+    .exec(productInformation())
+    .exitHereIfFailed
+    .exec(productDescription())
+    .exitHereIfFailed
+    .exec(addToCart())
+    .exitHereIfFailed
+    .exec(recommendedOTCProducts())
+    .exitHereIfFailed
+    .exec(savingsOnCart())
+    .exitHereIfFailed
+    .exec(sendOTP())
+    .exitHereIfFailed
+    .exec(login())
+    .exitHereIfFailed
+    .exec(cartSyncAfterLogin())
+    .exitHereIfFailed
     .pause(2 seconds)
-    .exec(cartItemsCountAfterLogin()).exitHereIfFailed
-    .exec(fetchAddressOrAddIfNotExist()).exitHereIfFailed
-    .exec(estimateDelivery()).exitHereIfFailed
-    .exec(deliveryPreference()).exitHereIfFailed
-    .exec(paymentInstrumentationBeforeOrderPlacement()).exitHereIfFailed
-    .exec(cartItems()).exitHereIfFailed
-    .exec(placeOrder()).exitHereIfFailed
-    .exec(fetchOrders()).exitHereIfFailed
-    .exec(orderDetails()).exitHereIfFailed
-    .exec(orderItems()).exitHereIfFailed
-    .exec(orderPriceFluctuation()).exitHereIfFailed
-    .exec(paymentInstrumentationAfterOrderPlacement()).exitHereIfFailed
+    .exec(cartItemsCountAfterLogin())
+    .exitHereIfFailed
+    .exec(fetchAddressOrAddIfNotExist())
+    .exitHereIfFailed
+    .exec(estimateDelivery())
+    .exitHereIfFailed
+    .exec(deliveryPreference())
+    .exitHereIfFailed
+    .exec(paymentInstrumentationBeforeOrderPlacement())
+    .exitHereIfFailed
+    .exec(cartItems())
+    .exitHereIfFailed
+    .exec(placeOrder())
+    .exitHereIfFailed
+    .exec(fetchOrders())
+    .exitHereIfFailed
+    .exec(orderDetails())
+    .exitHereIfFailed
+    .exec(orderItems())
+    .exitHereIfFailed
+    .exec(orderPriceFluctuation())
+    .exitHereIfFailed
+    .exec(paymentInstrumentationAfterOrderPlacement())
+    .exitHereIfFailed
     .exec(doIf(session => isCODPayment(session)) {
       exec(paymentInvocationDetails()).exitHereIfFailed
-        .exec(codPayment()).exitHereIfFailed
-        .exec(onlinePaymentStatusVerification()).exitHereIfFailed
-    }).exitHereIfFailed
-    .exec(cancelOrder()).exitHereIfFailed
+        .exec(codPayment())
+        .exitHereIfFailed
+        .exec(onlinePaymentStatusVerification())
+        .exitHereIfFailed
+    })
+    .exitHereIfFailed
+    .exec(cancelOrder())
+    .exitHereIfFailed
     .exec(orderDetails())
 
-
-  override def step(): OpenInjectionStep = rampUsers(1) during (2 seconds);
+  override def step(): OpenInjectionStep =
+    rampUsers(rampUpUsers) during (rampUpDuration seconds);
 
   override def enabled() = true
 
