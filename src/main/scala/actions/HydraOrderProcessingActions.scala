40ea91d7258435a242dc97e3332897fb38126928
Sarath Kumar Purushothaman <sarath.purushothaman@pharmeasy.in>
2020-05-13 16:17:04 +0530

    hydra

--- src/main/scala/actions/HydraOrderProcessingActions.scala
+++ src/main/scala/actions/HydraOrderProcessingActions.scala
@@ -1,14 +1,11 @@
 package actions
 
-import actions.ConsumerActions.getFromSession
-import actions.hydra.HydraOrderCreation.order
 import actions.hydra.{HydraOrderCreation, HydraOrderUpdate, HydraUpdate}
 import io.gatling.core.Predef.{jsonPath, _}
-import newUtilities.{TokenGeneration, newConfigManager}
-import io.gatling.core.Predef._
-import io.gatling.http.Predef._
 import io.gatling.core.session.Session
-import org.json4s.native.Serialization.write
+import io.gatling.http.Predef._
+import newUtilities.{TokenGeneration, newConfigManager}
+import simulations.templates.Feeders
 
 object HydraOrderProcessingActions {
 
@@ -16,12 +13,12 @@ object HydraOrderProcessingActions {
 
   def addToSession(session: Session, attributes: (String, Any)*) = session.setAll(attributes)
 
-  def CreateMarketPlaceOrder(baseUrl: String = newConfigManager.getString("hydra.base_url")) =
+  def createMarketPlaceOrder(baseUrl: String = newConfigManager.getString("hydra.base_url")) =
     http("Create Hydra Order")
       .post(session => baseUrl + "/orders")
-      .header("accept","application/json")
-      .header("contentType","application/json")
-      .header("Authorization",TokenGeneration.getDefaultToken())
+      .header("accept", "application/json")
+      .header("contentType", "application/json")
+      .header("Authorization", TokenGeneration.getDefaultToken())
       .body(StringBody(HydraOrderCreation.getOrderPayload()))
       .asJson
       .check(
@@ -30,30 +27,27 @@ object HydraOrderProcessingActions {
         jsonPath("$.retailerOrderId").saveAs("retailerOrderId"))
 
 
-  def GetOrderById(baseUrl: String = newConfigManager.getString("hydra.base_url")) =
-    http("Create Hydra Order")
-      .get(session => baseUrl + "/orders/"+ getFromSession(session,"retailerOrderId"))
-      .header("accept","application/json")
-      .header("contentType","application/json")
-      .header("Authorization",TokenGeneration.getDefaultToken())
+  def getOrderById(baseUrl: String = newConfigManager.getString("hydra.base_url")) =
+    http("Fetch Hydra Order")
+      .get(session => baseUrl + "/orders/" + getFromSession(session, Feeders.fetchKey))
+      .header("accept", "application/json")
+      .header("contentType", "application/json")
+      .header("Authorization", TokenGeneration.getDefaultToken())
       .asJson
       .check(
         status.is(200),
         jsonPath("$.status").saveAs("status"))
 
 
-  def UpdateOrder(baseUrl: String = newConfigManager.getString("hydra.base_url"),
-                  orderId: String,hydraUpdate: HydraUpdate) =
-    http("Create Hydra Order")
-      .get(session => baseUrl + "/orders/rb/"+getFromSession(session,"retailerOrderId")+"/status")
-      .header("accept","application/json")
-      .header("contentType","application/json")
-      .header("Authorization",TokenGeneration.getDefaultToken())
+  def updateOrder(baseUrl: String = newConfigManager.getString("hydra.base_url"),
+                  orderId: String, hydraUpdate: HydraUpdate) =
+    http("Update Hydra Order")
+      .get(session => baseUrl + "/orders/rb/" + getFromSession(session, "retailerOrderId") + "/status")
+      .header("accept", "application/json")
+      .header("contentType", "application/json")
+      .header("Authorization", TokenGeneration.getDefaultToken())
       .body(StringBody(HydraOrderUpdate.getRandomUpdateScenario()))
       .asJson
       .check(
         status.is(200))
-
-
-
 }
--- src/main/scala/actions/hydra/HydraOrderCreation.scala
+++ src/main/scala/actions/hydra/HydraOrderCreation.scala
@@ -1,9 +1,8 @@
 package actions.hydra
 
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.time.{LocalDate, LocalDateTime}
 
-import actions.scm.{Address, Item, OrderPayload}
 import newUtilities.newUtilities.{randomNumberBetweenRange, readCSV}
 import org.json4s.DefaultFormats
 import org.json4s.native.Serialization.write
@@ -19,9 +18,9 @@ object HydraOrderCreation {
     val shuffled = random.shuffle(hydraMedsData)
     val num = randomNumberBetweenRange(1, max)
     var ListOfItems = List[Items]()
-    for( a <- 0 to num-1){
+    for (a <- 0 to num - 1) {
       val data = shuffled(a);
-      val items  = Items(data(0),data(1),data(2),data(3),data(4),data(5),data(6),data(7),data(8))
+      val items = Items(data(0), data(1), data(2), data(3), data(4), data(5), data(6), data(7), data(8))
       ListOfItems = items :: ListOfItems
     }
     return ListOfItems
@@ -29,7 +28,7 @@ object HydraOrderCreation {
 
   val prescriptions = Seq("https://www.computerhope.com/jargon/a/array-of-pointers.jpg", "https://www.computerhope.com/jargon/s/special-characters.jpg")
 
-  def ItemsData():String = write(HydraItemsPayload())
+  def ItemsData(): String = write(HydraItemsPayload())
 
   val address = Address("89 6th cross",
     "RPS Road",
@@ -42,7 +41,7 @@ object HydraOrderCreation {
   val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
   val delivaryDate: String = LocalDateTime.now.plusDays(5).format(dateFormatter)
 
-  val order =  CreateOrder(
+  val order = CreateOrder(
     address,
     "LoadTest@gmail.com",
     "9999877778",
@@ -57,7 +56,8 @@ object HydraOrderCreation {
     "${retailerId}",
     "source-work-flow-id-LoadTest"
   )
-  def getOrderPayload():String = {
-    return write(order).replace("\"StringReplace\"","${items}")
+
+  def getOrderPayload(): String = {
+    return write(order).replace("\"StringReplace\"", "${items}")
   }
 }
