4e93caab95043ad7f1cdeadb1902637f9efbcd2f
mayank godiyal <mayank.godiyal@pharmeasy.com>
2025-07-15 16:34:38 +0530

    load testing cases for partner

--- src/test/scala/simulations/PartnerSimulation.scala
+++ src/test/scala/simulations/PartnerSimulation.scala
@@ -16,6 +16,7 @@ class PartnerSimulation extends Simulation {
   val scn15 = PartnerActions.scenarioWithCsv("partner_detail_15rps.csv", "15 RPS")
 
  setUp(
+
    scn5.inject(rampUsersPerSec(1) to 5 during (60.seconds), constantUsersPerSec(5) during (240.seconds)),
    scn10.inject(rampUsersPerSec(5) to 10 during (60.seconds), constantUsersPerSec(2) during (240.seconds)),
    scn15.inject(rampUsersPerSec(10) to 15 during (60.seconds), constantUsersPerSec(3) during (240.seconds))
